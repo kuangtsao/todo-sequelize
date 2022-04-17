b4e3354fcc210ecc2becda57478423026bc0fb27
Marcus Liu <me@mail.lmarcus.onl>
2022-04-17 21:41:34 +0800

    註冊功能

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const usePassport = require('./config/passport')
+const passport = require('passport')
+
 const app = express()
 const PORT = 3000
 const db = require('./models')
@@ -10,9 +14,17 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -27,9 +39,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
